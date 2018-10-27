18dda0a9443b8854329706be08be8fae01fe8b48
Alexander Bayandin <a.bayandin@gmail.com>
2018-10-27 15:33:09 +0100

    Restore layout

--- app/src/main/java/in/bayand/newversionavailable/MainActivity.kt
+++ app/src/main/java/in/bayand/newversionavailable/MainActivity.kt
@@ -2,11 +2,14 @@ package `in`.bayand.newversionavailable
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        versionName.text = BuildConfig.VERSION_NAME
     }
 }
